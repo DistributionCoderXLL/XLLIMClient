3142055c256bd2520a80506735c6519758baa3d0
iOS-肖乐乐 <xiaoll@bbdtek.com>
2018-04-16 15:44:58 +0800

    02

--- XLLIMChat/XLLIMChat/AppDelegate/AppDelegate.m
+++ XLLIMChat/XLLIMChat/AppDelegate/AppDelegate.m
@@ -20,7 +20,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    //初始化SDK-新功能
+    //初始化SDK-新功能02
     [XLLSDKConfig initializeSDK];
     
     self.window.backgroundColor = [UIColor whiteColor];
